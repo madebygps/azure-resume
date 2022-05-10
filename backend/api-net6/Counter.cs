220ce9d635d64aea6b4a0fc61413a55e336bbf24
Gwyneth Pena <gwynethpena@outlook.com>
2022-05-10 18:48:20 -0400

    Added code for dotnet6 api

---
+++ backend/api-net6/Counter.cs
@@ -0,0 +1,14 @@
+using System.Text.Json.Serialization;
+
+namespace Company.Function
+{
+    public class Counter 
+    {
+        
+        [JsonPropertyName("id")]
+        public string Id {get; set;}
+        
+        [JsonPropertyName("count")]
+        public int Count {get;set;}
+    }
+}
---
+++ backend/api-net6/GetVisitorCounter.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public class GetVisitorCounter
+    {
+        private readonly ILogger _logger;
+
+        public GetVisitorCounter(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<GetVisitorCounter>();
+        }
+
+        [Function("GetVisitorCounter")]
+        public MyOutputType Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+        [CosmosDBInput(databaseName: "CloudResume", collectionName: "Counter", ConnectionStringSetting = "CosmosDbConnectionString", Id = "index",
+                PartitionKey = "index")] Counter input)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            
+            response.Headers.Add("Content-Type", "application/json; charset=utf-8");
+
+            string jsonString = JsonSerializer.Serialize(input);
+
+            response.WriteString(jsonString);
+
+            return new MyOutputType()
+            {
+                Counter = new Counter
+                {
+                    Id = "index",
+                    Count = input.Count + 1
+                },
+
+                HttpResponse = response
+            };
+        }
+    }
+
+
+    public class MyOutputType
+    {
+        [CosmosDBOutput(databaseName: "CloudResume", collectionName: "Counter", ConnectionStringSetting = "CosmosDbConnectionString")]
+        public Counter Counter { get; set; }
+        public HttpResponseData HttpResponse { get; set; }
+    }
+}
